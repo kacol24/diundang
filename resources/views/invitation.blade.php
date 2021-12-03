3bf0be35efc3421f3f5ca9371ca33ab8e422480b
Kevin Chandra <kacol.bot@gmail.com>
2021-12-03 17:34:24 +0700

    generate qr code

---
+++ resources/views/invitation.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en" class="h-100">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>You Are Invited - The Wedding Of John Doe & Jane Doe</title>
+    <style>
+        svg {
+            max-width: 100%;
+            height: auto;
+        }
+    </style>
+</head>
+<body class="h-100 d-flex align-items-center">
+<div class="container">
+    <div class="card mx-auto" style="max-width: 500px;">
+        <div class="card-body">
+            {!! QrCode::size(500)->generate($invitation->guest_code) !!}
+        </div>
+        <div class="card-body text-center">
+            <h5 class="card-title">
+                {{ $invitation->name }}
+            </h5>
+            <p class="card-text">
+                {{ $invitation->seating->name }}<br>
+                Pax: {{ $invitation->guests }}
+            </p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Invitation;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $guestCode = request('guest');
+    $invitation = Invitation::where('guest_code', $guestCode)->first();
+
+    return view('invitation', compact('invitation'));
 });
