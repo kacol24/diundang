a14e8be0ab36859aa95ad25ba55acfe747e09189
Kevin Chandra <kacol.bot@gmail.com>
2021-12-03 18:51:29 +0700

    invitation card design

--- resources/views/invitation.blade.php
+++ resources/views/invitation.blade.php
@@ -18,20 +18,19 @@
     </style>
 </head>
 <body class="h-100 d-flex align-items-center">
-<div class="container">
-    <div class="card mx-auto" style="max-width: 500px;">
-        <div class="card-body">
-            {!! QrCode::size(500)->generate($invitation->guest_code) !!}
-        </div>
-        <div class="card-body text-center">
-            {{ $invitation->guest_code }}
-            <h5 class="card-title">
-                {{ $invitation->name }}
-            </h5>
-            <p class="card-text">
-                {{ $invitation->guests }} {{ Str::plural('guest', $invitation->guests) }}<br>
-                {{ $invitation->seating->name }}<br>
-            </p>
+<div class="mx-auto position-relative" style="max-width: 500px">
+    <img src="{{ asset('images/design.jpg') }}" class="img-fluid">
+    <div class="position-absolute" style="left: 50%;transform: translateX(-50%);top: 46%;width: 40%;">
+        <div class="card w-100 mx-auto rounded-3">
+            <div class="card-body text-center w-100">
+                {!! QrCode::size(500)->generate($invitation->guest_code) !!}
+                <h5 class="card-title mt-2 mb-0">
+                    {{ $invitation->name }}
+                </h5>
+                <p class="card-text m-0">
+                    {{ $invitation->guests }} {{ Str::plural('guest', $invitation->guests) }}
+                </p>
+            </div>
         </div>
     </div>
 </div>
