b5f351ffc2a07182dc06bbf99e3434688178d548
Kevin Chandra <kacol.bot@gmail.com>
2021-12-04 15:59:28 +0700

    stream endpoint

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,21 @@ Route::get('/', function () {
     return $pdf->download('Invitation, The Wedding of Kevin & Fernanda, 09-10-2022' . $guestCode.'.pdf');
 });
 
+Route::get('/download', function () {
+    $guestCode = request('guest');
+    $invitation = Invitation::where('guest_code', $guestCode)->first();
+
+    QrCode::size(500)
+          ->format('png')
+          ->generate($invitation->guest_code, storage_path('app/public/qr/'.$guestCode.'.png'));
+    $qr = storage_path('app/public/qr/'.$guestCode.'.png');
+
+    $pdf = App::make('dompdf.wrapper');
+    $pdf->loadView('qr', compact('invitation', 'qr'));
+
+    return $pdf->stream('Invitation, The Wedding of Kevin & Fernanda, 09-10-2022'.$guestCode.'.pdf');
+});
+
 Route::get('/qr', function () {
     $guestCode = request('guest');
     $invitation = Invitation::where('guest_code', $guestCode)->first();
