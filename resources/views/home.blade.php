688fff9efbcb8904992e1f2e20048c7337c8cbcd
Kevin Chandra <kacol.bot@gmail.com>
2022-01-31 01:07:03 +0700

    update home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,6 +14,15 @@
     <title>Hello, world!</title>
 
     <style>
+        :root {
+            --color-primary: #09232f;
+            --color-secondary: #4d1c32;
+        }
+
+        .rounded-4 {
+            border-radius: 1rem;
+        }
+
         .hero-image {
             height: 100vh;
             position: relative;
@@ -23,7 +32,11 @@
             justify-content: space-between;
         }
 
-        .hero-image::before {
+        .darkened-overlay {
+            position: relative;
+        }
+
+        .darkened-overlay::before {
             content: '';
             position: absolute;
             z-index: 0;
@@ -31,19 +44,40 @@
             left: 0;
             width: 100%;
             height: 100%;
-            background-color: rgba(0, 0, 0, .7);
+            background-color: rgba(0, 0, 0, .5);
+        }
+
+        .btn-primary {
+            background-color: var(--color-primary);
+            border-color: var(--color-primary);
+        }
+
+        .btn-primary:hover {
+            background-color: #0d3344;
+            border-color: #0d3344;
+        }
+
+        .btn-secondary {
+            background-color: var(--color-secondary);
+            border-color: var(--color-secondary);
+        }
+
+        .btn-secondary:hover {
+            background-color: #3a1526;
+            border-color: #3a1526;
         }
 
         .link-color\:blue {
-            color: #545b8c;
+            color: var(--color-primary);
+            transition: color 200ms;
         }
 
         .link-color\:blue:hover {
-            color: #8c92ba;
+            color: #0d3344;
         }
 
         .text-color\:blue {
-            color: #545b8c;
+            color: var(--color-primary);
         }
 
         .text-color\:gold {
@@ -61,33 +95,30 @@
             z-index: 10;
         }
     </style>
+    <script src="https://cdn.jsdelivr.net/npm/lozad@1.16.0/dist/lozad.min.js"></script>
 </head>
 <body>
 
 <div class="wrapper">
-    <div class="hero-image py-5" data-parallax="scroll" data-image-src="{{ asset('images/hero-1.jpeg') }}">
+    <div class="hero-image darkened-overlay text-center py-5" data-parallax="scroll"
+         data-image-src="{{ asset('images/hero-2.jpg') }}">
         <img src="{{ asset('images/logo-initials.png') }}" alt="" class="img-fluid" width="200" style="z-index: 1">
-        <a href="" class="link-color:blue text-decoration-none" style="z-index: 1">
-            <i class="fas fa-fw fa-chevron-circle-down fa-2x"></i>
-        </a>
-        <div class="position-absolute text-center d-flex align-items-center justify-content-center" style="top: 0;left: 0;width: 100%;height: 100%;">
-            <div class="text-color:gold font-cursive">
-                <h3 data-aos="zoom-out-down">
-                    The Wedding of
-                </h3>
-                <h1 class="display-1" data-aos="zoom-out-up" data-aos-delay="100" data-aos-duration="1000">
-                    Kevin & Fernanda
-                </h1>
-                <div data-aos="fade-up" data-aos-delay="600">
-                    Save The Date<br>
-                    -<br>
-                    24 . 09 . 2022
-                </div>
-            </div>
+        <div class="text-white font-cursive">
+            <h3 data-aos="zoom-out-down">
+                The Wedding of
+            </h3>
+            <h1 class="display-1" data-aos="zoom-out-up" data-aos-delay="100" data-aos-duration="1000">
+                Kevin & Fernanda
+            </h1>
+        </div>
+        <div data-aos="fade-up" data-aos-delay="300" class="text-white">
+            Save The Date<br>
+            -<br>
+            24 . 09 . 2022
         </div>
     </div>
     <header class="sticky-top">
-        <nav class="navbar navbar-light navbar-expand-lg bg-light">
+        <nav class="navbar navbar-dark navbar-expand-lg" style="background-color:var(--color-secondary);">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">
                     #loVINyouforeFER
@@ -113,7 +144,7 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="#event_date">Save The Date</a>
                             </li>
-{{--                            <li class="nav-item dropdown">--}}
+                            {{--                            <li class="nav-item dropdown">--}}
                             {{--                                <a class="nav-link dropdown-toggle" href="#" id="offcanvasNavbarDropdown" role="button"--}}
                             {{--                                   data-bs-toggle="dropdown" aria-expanded="false">--}}
                             {{--                                    Dropdown--}}
@@ -133,53 +164,63 @@
             </div>
         </nav>
     </header>
-    <div class="py-5" id="couple" style="background-color:#545b8c;">
-        <div class="container text-color:gold">
-            <div class="row align-items-center overflow-hidden">
-                <div class="col-md">
-                    <div class="text-center" data-aos="fade-right">
-                        <img src="https://placekitten.com/250" alt="" class="img-fluid rounded-circle mb-3"
-                             style="max-width: 250px" data-aos="flip-right" data-aos-delay="300">
-                        <h2 class="font-cursive">
-                            Kevin Chandra
-                        </h2>
-                        <p>
-                            <strong>Son of</strong><br>
-                            Gunawan Chandra & Susilowati
-                        </p>
+    <div class="py-5" id="couple" style="background-color:var(--color-primary);">
+        <div class="container py-5">
+            <div class="row py-5 align-items-center justify-content-around overflow-hidden">
+                <div class="col-md-4">
+                    <div class="card rounded-4 text-center" data-aos="fade-right">
+                        <div class="card-body py-5">
+                            <img src="https://placekitten.com/250" alt="" class="img-fluid rounded-circle mb-3 mx-auto"
+                                 style="max-width: 150px" data-aos="flip-right" data-aos-delay="300">
+                            <h2 class="font-cursive">
+                                Kevin Chandra
+                            </h2>
+                            <p class="m-0">
+                                <strong>Son of</strong><br>
+                                Gunawan Chandra & Susilowati
+                            </p>
+                        </div>
                     </div>
                 </div>
-                <div class="col-md-1 display-1 text-center py-5" style="font-family: cursive;">
-                    &
-                </div>
-                <div class="col-md">
-                    <div class="text-center" data-aos="fade-left">
-                        <img src="https://placekitten.com/300" alt="" class="img-fluid rounded-circle mb-3"
-                             style="max-width: 250px" data-aos="flip-left" data-aos-delay="300">
-                        <h2 class="font-cursive">
-                            Fernanda Eka Putri
-                        </h2>
-                        <p>
-                            <strong>Daughter of</strong><br>
-                            Ge Cingkai & Lim Liauw Hung San
-                        </p>
+                <div class="col-md-4">
+                    <div class="card rounded-4 text-center" data-aos="fade-left" data-aos-delay="300">
+                        <div class="card-body py-5">
+                            <img src="https://placekitten.com/300" alt="" class="img-fluid rounded-circle mb-3 mx-auto"
+                                 style="max-width: 150px" data-aos="flip-left" data-aos-delay="300">
+                            <h2 class="font-cursive">
+                                Fernanda Eka Putri
+                            </h2>
+                            <p class="m-0">
+                                <strong>Daughter of</strong><br>
+                                Ge Cingkai & Lim Liauw Hung San
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="py-5" id="event_date" style="background-color:rgba(255,161,161,0.3);">
-        <div class="container">
-            <h2 class="font-cursive display-3 text-center text-color:blue">
+    <div id="save_the_date" class="py-5 position-relative">
+        <div class="position-absolute w-100 darkened-overlay" style="top: 0;left: 0;">
+            <img src="{{ asset('images/banner-save-the-date.jpg') }}"
+                 class="img-fluid" data-lazy>
+        </div>
+        <div class="container py-5 position-relative text-center" style="z-index: 1">
+            <h3 class="text-uppercase text-white display-4">
+                Saturday,<br>
+                September 24th, 2022
+            </h3>
+            <a href="" class="btn btn-secondary" data-aos="fade-down">
                 Save The Date
-            </h2>
-            <div class="card border-0 rounded-3 shadow-sm">
-                <div class="row align-items-center">
-                    <div class="col-md-5 text-center overflow-hidden">
-                        <div class="p-5 text-color:blue" data-aos="fade-right">
-                            <h3 class="font-cursive">
+            </a>
+            <div class="row mt-5 justify-content-around">
+                <div class="col-md-5">
+                    <div class="card rounded-4 text-center border-0" data-aos="fade-up">
+                        <div class="card-body py-5">
+                            <h3 class="card-title display-6">
                                 Holy Matrimony
                             </h3>
+                            <hr>
                             <strong>11:00</strong>
                             <div class="text-muted">
                                 at
@@ -190,22 +231,14 @@
                             </address>
                         </div>
                     </div>
-                    <div class="col-md">
-                        <div class="ratio ratio-16x9">
-                            <iframe
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15830.915984586807!2d112.65947088471673!3d-7.27164828084286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fea1a306000b%3A0x6d92118a59928e4d!2sCatholic%20Church%20of%20St%20Aloysius%20Gonzaga!5e0!3m2!1sen!2sid!4v1643106758467!5m2!1sen!2sid"
-                                style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-                        </div>
-                    </div>
                 </div>
-            </div>
-            <div class="card border-0 rounded-3 shadow-sm mt-5">
-                <div class="row align-items-center">
-                    <div class="col-md-5 text-center order-md-3 overflow-hidden">
-                        <div class="p-5 text-color:blue" data-aos="fade-left">
-                            <h3 class="font-cursive">
+                <div class="col-md-5">
+                    <div class="card rounded-4 text-center border-0" data-aos="fade-up" data-aos-delay="300">
+                        <div class="card-body py-5">
+                            <h3 class="card-title display-6">
                                 Wedding Reception
                             </h3>
+                            <hr>
                             <strong>18:00</strong>
                             <div class="text-muted">
                                 at
@@ -214,13 +247,6 @@
                                 <strong>XO Palace</strong><br>
                                 Jl. something bla di daerah bla
                             </address>
-                        </div>
-                    </div>
-                    <div class="col-md order-md-1">
-                        <div class="ratio ratio-16x9">
-                            <iframe
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.640324380142!2d112.70500521525437!3d-7.281701673585097!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fc0ee483ffff%3A0x625015abf9f3022e!2sXO%20Palace!5e0!3m2!1sen!2sid!4v1643106845396!5m2!1sen!2sid"
-                                style="border:0;" allowfullscreen="" loading="lazy"></iframe>
                         </div>
                     </div>
                 </div>
@@ -248,6 +274,12 @@
 <script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.min.js"></script>
 <script>
     AOS.init();
+
+    var lazyParallax = window.lozad('.parallax-slider');
+    lazyParallax.observe();
+
+    const observer = window.lozad('[data-lazy]');
+    observer.observe();
 </script>
 </body>
 </html>
