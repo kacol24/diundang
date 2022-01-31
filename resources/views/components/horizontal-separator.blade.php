23b7f0dbcf3c3c174f26b404edaca03d063c8cff
Kevin Chandra <kacol.bot@gmail.com>
2022-01-31 12:23:38 +0700

    home

---
+++ resources/views/components/horizontal-separator.blade.php
@@ -0,0 +1,3 @@
+<div class="d-block">
+    <img src="{{ asset('images/horizontal-separator.png') }}" alt="horizontal separator" class="img-fluid mx-auto">
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -96,25 +96,22 @@
         }
     </style>
     <script src="https://cdn.jsdelivr.net/npm/lozad@1.16.0/dist/lozad.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.8.1/dist/cdn.min.js" defer></script>
 </head>
 <body>
 
 <div class="wrapper">
     <div class="hero-image darkened-overlay text-center py-5" data-parallax="scroll"
          data-image-src="{{ asset('images/hero-2.jpg') }}">
-        <img src="{{ asset('images/logo-initials.png') }}" alt="" class="img-fluid" width="200" style="z-index: 1">
+        <img src="{{ asset('images/logo-initials.png') }}" alt="" class="img-fluid" width="200" style="z-index: 1"
+             data-aos="fade-down">
         <div class="text-white font-cursive">
-            <h3 data-aos="zoom-out-down">
-                The Wedding of
-            </h3>
-            <h1 class="display-1" data-aos="zoom-out-up" data-aos-delay="100" data-aos-duration="1000">
+            <h1 class="display-1" data-aos="zoom-out-up" data-aos-duration="1000">
                 Kevin & Fernanda
             </h1>
-        </div>
-        <div data-aos="fade-up" data-aos-delay="300" class="text-white">
-            Save The Date<br>
-            -<br>
-            24 . 09 . 2022
+            <h3 data-aos="zoom-out-down" data-aos-offset="-100" data-aos-delay="100">
+                The Wedding
+            </h3>
         </div>
     </div>
     <header class="sticky-top">
@@ -171,7 +168,7 @@
                     <div class="card rounded-4 text-center" data-aos="fade-right">
                         <div class="card-body py-5">
                             <img src="https://placekitten.com/250" alt="" class="img-fluid rounded-circle mb-3 mx-auto"
-                                 style="max-width: 150px" data-aos="flip-right" data-aos-delay="300">
+                                 style="max-width: 150px" data-aos="fade-down" data-aos-delay="300">
                             <h2 class="font-cursive">
                                 Kevin Chandra
                             </h2>
@@ -183,10 +180,10 @@
                     </div>
                 </div>
                 <div class="col-md-4">
-                    <div class="card rounded-4 text-center" data-aos="fade-left" data-aos-delay="300">
+                    <div class="card rounded-4 text-center" data-aos="fade-left">
                         <div class="card-body py-5">
                             <img src="https://placekitten.com/300" alt="" class="img-fluid rounded-circle mb-3 mx-auto"
-                                 style="max-width: 150px" data-aos="flip-left" data-aos-delay="300">
+                                 style="max-width: 150px" data-aos="fade-down" data-aos-delay="300">
                             <h2 class="font-cursive">
                                 Fernanda Eka Putri
                             </h2>
@@ -200,12 +197,11 @@
             </div>
         </div>
     </div>
-    <div id="save_the_date" class="py-5 position-relative">
-        <div class="position-absolute w-100 darkened-overlay" style="top: 0;left: 0;">
-            <img src="{{ asset('images/banner-save-the-date.jpg') }}"
-                 class="img-fluid" data-lazy>
-        </div>
-        <div class="container py-5 position-relative text-center" style="z-index: 1">
+    <div id="save_the_date" class="position-relative pb-5" style="padding-top: 150px;">
+        <div class="position-absolute w-100 darkened-overlay"
+             style="top: 0;left: 0; height: 80%; background-repeat: no-repeat; background-size: cover; background-position: center bottom"
+             data-lazy data-background-image="{{ asset('images/banner-save-the-date.jpg') }}"></div>
+        <div class="container position-relative text-center" style="z-index: 1">
             <h3 class="text-uppercase text-white display-4">
                 Saturday,<br>
                 September 24th, 2022
@@ -220,8 +216,10 @@
                             <h3 class="card-title display-6">
                                 Holy Matrimony
                             </h3>
-                            <hr>
-                            <strong>11:00</strong>
+                            @include('components.horizontal-separator')
+                            <strong class="mt-3 d-block">
+                                11:00
+                            </strong>
                             <div class="text-muted">
                                 at
                             </div>
@@ -238,8 +236,10 @@
                             <h3 class="card-title display-6">
                                 Wedding Reception
                             </h3>
-                            <hr>
-                            <strong>18:00</strong>
+                            @include('components.horizontal-separator')
+                            <strong class="mt-3 d-block">
+                                18:00
+                            </strong>
                             <div class="text-muted">
                                 at
                             </div>
@@ -249,6 +249,157 @@
                             </address>
                         </div>
                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="gallery" class="py-5">
+        <div class="container">
+            <img src="{{ asset('images/hero-2.jpg') }}" alt="" class="img-fluid w-100 mb-4">
+            <img src="{{ asset('images/hero-2.jpg') }}" alt="" class="img-fluid w-100">
+        </div>
+    </div>
+    <div id="protocol" style="background-color:var(--color-primary);" class="rounded-4">
+        <div class="container text-white py-5">
+            <p class="text-center mx-auto" style="max-width: 350px">
+                Love is caring.<br>
+                Your health and safety is very important to us.<br>
+                To keep everyone comfortable and safe,
+                please follow health protocols.
+            </p>
+            <div class="row mt-5">
+                <div class="col-6 col-md text-center">
+                    <figure class="figure">
+                        <img src="{{ asset('images/protocol-wear-mask.png') }}" alt="" class="img-fluid figure-img">
+                        <figcaption class="figure-caption text-white">
+                            <div class="lead">
+                                Wear Mask
+                            </div>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="col-6 col-md text-center">
+                    <figure class="figure">
+                        <img src="{{ asset('images/protocol-wear-mask.png') }}" alt="" class="img-fluid figure-img">
+                        <figcaption class="figure-caption text-white">
+                            <div class="lead">
+                                Wash Hands
+                            </div>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="col-6 col-md text-center">
+                    <figure class="figure">
+                        <img src="{{ asset('images/protocol-wear-mask.png') }}" alt="" class="img-fluid figure-img">
+                        <figcaption class="figure-caption text-white">
+                            <div class="lead">
+                                Clean Surfaces
+                            </div>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="col-6 col-md text-center">
+                    <figure class="figure">
+                        <img src="{{ asset('images/protocol-wear-mask.png') }}" alt="" class="img-fluid figure-img">
+                        <figcaption class="figure-caption text-white">
+                            <div class="lead">
+                                Keep Distance
+                            </div>
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="gift" style="height: 100vh;" class="d-flex align-items-center justify-content-center">
+        <div class="container text-center">
+            <h1 class="text-uppercase">
+                Wedding Gift
+            </h1>
+            @include('components.horizontal-separator')
+            <button class="btn btn-secondary text-uppercase mt-4" data-bs-toggle="modal">
+                Send Gift
+            </button>
+        </div>
+    </div>
+    <div id="cta"
+         style="background-repeat: no-repeat; background-size: cover; background-position: center bottom; padding-top: 10%; padding-bottom: 10%"
+         data-parallax="scroll" data-image-src="{{ asset('images/banner-cta.jpg') }}">
+        <div class="container text-white py-5 my-5 text-center">
+            <h3 class="display-3 text-uppercase">
+                We Can't Wait To See You!
+            </h3>
+            <em>
+                Please let us know if you'll be able to make it.
+            </em>
+        </div>
+    </div>
+    <div id="rsvp" class="my-5 py-5">
+        <div class="container my-5 py-5">
+            <div class="text-center">
+                <h1>
+                    RSVP
+                </h1>
+                @include('components.horizontal-separator')
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <form action="" class="mt-4"
+                          x-data="{
+                            attend: ''
+                          }">
+                        <div class="mb-3">
+                            <label class="fw-bold">
+                                Your Name
+                            </label>
+                            <input type="text" readonly class="form-control-plaintext" id="staticEmail"
+                                   value="Nama Tamu Undangan"
+                                   aria-label="your name">
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label class="fw-bold">
+                                        Hadir Resepsi
+                                    </label>
+                                    <div>
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" type="radio" name="inlineRadioOptions"
+                                                   id="inlineRadio1"
+                                                   value="yes" x-model="attend">
+                                            <label class="form-check-label" for="inlineRadio1">Hadir</label>
+                                        </div>
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" type="radio" name="inlineRadioOptions"
+                                                   id="inlineRadio2"
+                                                   value="no" x-model="attend">
+                                            <label class="form-check-label" for="inlineRadio2">Tidak</label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <div x-show="attend === 'yes'" x-transition>
+                                        <label class="fw-bold">
+                                            Jumlah Yang Hadir
+                                        </label>
+                                        <select class="form-select" aria-label="Default select example">
+                                            <option selected value="" disabled hidden>Jumlah Yang Hadir</option>
+                                            <option value="1">One</option>
+                                            <option value="2">Two</option>
+                                            <option value="3">Three</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-secondary">
+                                Confirm
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
