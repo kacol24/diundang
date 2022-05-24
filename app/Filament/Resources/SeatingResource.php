ce47454dc26eea50f855bd223680c3c427c0cb9d
Kevin Chandra <kacol.bot@gmail.com>
2022-05-24 23:01:27 +0700

    seating crud

---
+++ app/Filament/Resources/SeatingResource.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SeatingResource\Pages;
+use App\Filament\Resources\SeatingResource\RelationManagers;
+use App\Models\Seating;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+
+class SeatingResource extends Resource
+{
+    protected static ?string $model = Seating::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+            ])
+            ->filters([
+                //
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageSeatings::route('/'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/SeatingResource/Pages/EditSeating.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\SeatingResource\Pages;
+
+use App\Filament\Resources\SeatingResource;
+use Filament\Resources\Pages\EditRecord;
+
+class EditSeating extends EditRecord
+{
+    protected static string $resource = SeatingResource::class;
+}
