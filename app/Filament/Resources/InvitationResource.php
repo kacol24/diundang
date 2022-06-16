6fbe7d15a6c0de1c486a572a4fb088091f027e9e
Kevin Chandra <kacol.bot@gmail.com>
2022-06-16 20:06:34 +0700

    invitation form new layout

--- app/Filament/Resources/InvitationResource.php
+++ app/Filament/Resources/InvitationResource.php
@@ -5,11 +5,15 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\InvitationResource\Pages;
 use App\Filament\Resources\InvitationResource\RelationManagers;
 use App\Models\Invitation;
+use App\Models\Seating;
 use Filament\Forms;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Section;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
 use Filament\Tables;
+use Illuminate\Database\Eloquent\Model;
 
 class InvitationResource extends Resource
 {
@@ -20,28 +24,10 @@ class InvitationResource extends Resource
     public static function form(Form $form): Form
     {
         return $form
-            ->columns(1)
-            ->schema([
-                Forms\Components\Grid::make()
-                                     ->schema([
-                                         Forms\Components\TextInput::make('guest_code')
-                                                                   ->required(),
-                                         Forms\Components\BelongsToSelect::make('seating_id')
-                                                                         ->relationship('seating', 'name')
-                                                                         ->searchable()
-                                                                         ->required(),
-                                     ]),
-                Forms\Components\TextInput::make('name')
-                                          ->required(),
-                Forms\Components\TextInput::make('phone')
-                                          ->required()
-                                          ->type('tel')
-                                          ->prefix('+62'),
-                Forms\Components\TextInput::make('guests')
-                                          ->required()
-                                          ->type('number')
-                                          ->suffix('person(s)')
-                                          ->minValue(1),
+            ->schema(static::getFormSchema())
+            ->columns([
+                'sm' => 3,
+                'lg' => null,
             ]);
     }
 
@@ -74,6 +60,51 @@ class InvitationResource extends Resource
             'index'  => Pages\ListInvitations::route('/'),
             'create' => Pages\CreateInvitation::route('/create'),
             'edit'   => Pages\EditInvitation::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getFormSchema()
+    {
+        return [
+            Group::make()
+                 ->schema([
+                     Section::make('Guest Detail')
+                            ->schema([
+                                Forms\Components\TextInput::make('name')
+                                                          ->required(),
+                                Forms\Components\TextInput::make('phone')
+                                                          ->required()
+                                                          ->unique(ignorable: fn(?Model $record): ?Model => $record)
+                                                          ->type('tel')
+                                                          ->prefix('+62'),
+                            ]),
+                     Section::make('Invitation Detail')
+                            ->schema([
+                                Forms\Components\TextInput::make('guest_code')
+                                                          ->unique(ignorable: fn(?Model $record): ?Model => $record),
+                                Forms\Components\Select::make('seating_id')
+                                                       ->label('Table')
+                                                       ->options(Seating::all()->pluck('name', 'id'))
+                                                       ->searchable(),
+                                Forms\Components\TextInput::make('guests')
+                                                          ->required()
+                                                          ->type('number')
+                                                          ->suffix('person(s)')
+                                                          ->minValue(1),
+                            ]),
+                 ])->columnSpan([
+                    'sm' => 2,
+                ]),
+            Group::make()
+                 ->schema([
+                     Section::make('RSVP Detail')
+                            ->schema([
+                                Forms\Components\Toggle::make('is_attending'),
+                                Forms\Components\DateTimePicker::make('rsvp_at')
+                                                               ->label('RSVP At'),
+                            ]),
+                 ])
+                 ->columnSpan(1),
         ];
     }
 }
