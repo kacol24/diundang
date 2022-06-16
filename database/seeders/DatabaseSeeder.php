9a256bb2ed52e5de67732be29513969a491e3cf8
Kevin Chandra <kacol.bot@gmail.com>
2022-06-16 21:47:30 +0700

    seed default admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        User::create([
+            'name'     => 'Super Admin',
+            'email'    => 'admin@admin.com',
+            'password' => Hash::make('password'),
+        ]);
+
         $this->call(SeatingSeeder::class);
         $this->call(GroupSeeder::class);
     }
