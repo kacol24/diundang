215c912a19c78283224de23ada08c15d4a8aba38
Kevin Chandra <kacol.bot@gmail.com>
2022-06-16 17:50:37 +0700

    rsvp migations

---
+++ database/migrations/2022_06_15_170947_add_rsvp_fields_to_invitations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('invitations', function (Blueprint $table) {
+            $table->boolean('is_attending')->nullable()->after('seating_id');
+            $table->timestamp('rsvp_at')->nullable()->after('guests');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('invitations', function (Blueprint $table) {
+            $table->dropColumn(['is_attending', 'rsvp_at']);
+        });
+    }
+};
