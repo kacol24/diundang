066955b2044a6752a7f77428322b7c73b67ff21b
Kevin Chandra <kacol.bot@gmail.com>
2022-06-17 23:58:57 +0700

    add wedding date

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -3,6 +3,9 @@
         <div class="text-center">
             With ❤️<br>
             <h1 class="font-serif display-3">Kevin & Fernanda</h1>
+            <div class="text-muted mb-2">
+                24 . 09 . 2022
+            </div>
             <a href="https://www.instagram.com/explore/tags/loVINyouforeFER/" target="_blank"
                class="link-secondary">#loVINyouforeFER</a>
         </div>
