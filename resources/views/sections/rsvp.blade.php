a025a419a8ebf522b2dcec80f1d0df739acc5d59
Kevin Chandra <kacol.bot@gmail.com>
2022-06-17 23:58:39 +0700

    rsvp with register

--- resources/views/sections/rsvp.blade.php
+++ resources/views/sections/rsvp.blade.php
@@ -10,23 +10,31 @@
             <div class="col-md-6">
                 Please kindly help us prepare everything better by confirming your attendance to our wedding event
                 with the following RSVP form
-                <form action="{{ route('rsvp.store') }}" method="POST" class="mt-4 font-sans-serif"
+                <form action="{{ route('rsvp.store') }}" method="POST" class="mt-4 font-sans-serif" id="RsvpForm"
                       x-data="{
+                            name: '{{ $invitation->name ?? "" }}',
                             attend: '',
                             pax: ''
                           }">
                     @csrf
                     <div class="mb-3">
-                        <label class="fw-bold">
+                        <label class="fw-bold" for="name">
                             Your Name
                         </label>
-                        <input type="text" readonly class="form-control-plaintext" id="staticEmail"
+                        <input type="text"
+                               @if($invitation)
+                                   class="form-control-plaintext"
+                               readonly
                                value="{{ $guestName }}"
-                               aria-label="your name">
+                               @else
+                                   class="form-control"
+                               x-model="name"
+                               @endif
+                               id="name">
                     </div>
                     <div class="row">
                         <div class="col-md-6">
-                            <div class="mb-3">
+                            <div class="mb-5">
                                 <label class="fw-bold">
                                     Attend Reception
                                 </label>
@@ -51,16 +59,16 @@
                                 <div
                                     x-show="attend === 'yes'"
                                     x-transition>
-                                    <label class="fw-bold">
+                                    <label class="fw-bold" for="pax">
                                         How many people will attend?
                                     </label>
-                                    <select class="form-select">
+                                    <select class="form-select" id="pax"
+                                            x-model="pax">
                                         <option selected value="" disabled hidden>
                                             How many people will attend?
                                         </option>
                                         @foreach(range(1, $invitation->guests ?? 2) as $guest)
-                                            <option value="{{ $guest }}"
-                                                {{ $loop->last ? 'selected' : '' }}>
+                                            <option value="{{ $guest }}">
                                                 {{ $guest }} {{ \Str::plural('person', $guest) }}
                                             </option>
                                         @endforeach
@@ -80,9 +88,11 @@
                         <br>
                         <em>
                             * Having trouble with the RSVP form? Please contact <a
-                                href="https://wa.me/6282233662728?text={{ urlencode('Hi, this is '. $guestName .'! I want to confirm my attendance to your wedding reception.') }}">Kevin</a>
+                                href="https://wa.me/6282233662728?text={{ urlencode('Hi, this is '. $guestName .'! I want to confirm my attendance to your wedding reception.') }}"
+                                style="color: var(--color-secondary)">Kevin</a>
                             or <a
-                                href="https://wa.me/6282244872421?text={{ urlencode('Hi, '. $guestName .'! I want to confirm my attendance to your wedding reception.') }}">Fernanda</a>
+                                href="https://wa.me/6282244872421?text={{ urlencode('Hi, '. $guestName .'! I want to confirm my attendance to your wedding reception.') }}"
+                                style="color: var(--color-secondary)">Fernanda</a>
                             directly to confirm your attendance.
                         </em>
                     </small>
