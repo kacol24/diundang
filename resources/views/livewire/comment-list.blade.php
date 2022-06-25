46c88345dfdd45fb449fb2456e227b2904c31fa2
Kevin Chandra <kacol.bot@gmail.com>
2022-06-25 12:18:58 +0700

    fix polling issue

--- resources/views/livewire/comment-list.blade.php
+++ resources/views/livewire/comment-list.blade.php
@@ -1,22 +1,25 @@
-<div class="list-group mt-5" style="max-height: 500px; overflow-y: auto;"
-     wire:poll.{{ 15 * 1000 }}ms.visible>
-    @forelse($comments as $comment)
-        <div class="mb-4 mb-md-5">
-            <div class="d-flex w-100 justify-content-between">
-                <h5 class="mb-1 h6">
-                    {{ $comment->name }}
-                </h5>
-                <small>
-                    {{ $comment->created_at->diffForHumans() }}
+<div>
+    <div class="list-group mt-5" style="max-height: 500px; overflow-y: auto;"
+         wire:poll>
+        @forelse($comments as $comment)
+            <div class="mb-4 mb-md-5">
+                <div class="d-flex w-100 justify-content-between">
+                    <h5 class="mb-1 h6">
+                        {{ $comment->name }}
+                    </h5>
+                    <small>
+                        {{ $comment->created_at->diffForHumans() }}
+                    </small>
+                </div>
+                <small class="mb-1">
+                    {!! $comment->message !!}
                 </small>
             </div>
-            <small class="mb-1">
-                {!! $comment->message !!}
-            </small>
-        </div>
-    @empty
-        <em>
-            Be the first to leave a wish
-        </em>
-    @endforelse
+        @empty
+            <em>
+                Be the first to leave a wish
+            </em>
+        @endforelse
+    </div>
+
 </div>
