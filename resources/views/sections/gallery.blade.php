fd7908ccd3e61e92da4fed4f1331937290fba6ed
Kevin Chandra <kacol.bot@gmail.com>
2022-06-27 23:11:49 +0700

    disable original gallery

--- resources/views/sections/gallery.blade.php
+++ resources/views/sections/gallery.blade.php
@@ -1,65 +1,64 @@
 <div id="gallery" class="py-3">
-    <div class="container">
-        <div class="row grid" id="gallery_lightbox">
-            @foreach([
-                'ROB00520.jpg',
-                'gallery.jpeg',
-                'ROB00812.jpg',
-                'ROB00667.jpg',
-                'ROB00398.jpg',
-            ] as $gallery)
-                <div class="col-md-4 grid-item">
-                    <figure class="figure figure-credit">
-                        <a href="{{ asset('images/gallery/' . $gallery) }}"
-                           class="link-secondary text-decoration-none"
-                           data-fancybox="gallery"
-                           data-caption='<span style="font-size: 8px">{{ __('Photo by') }} - <a href="https://www.instagram.com/explore/tags/summerstoryrobert/" target="_blank" class="link-light">#summerstoryrobert</a></span>'>
-                            <img src="{{ asset('images/gallery/' . $gallery) }}"
-                                 class="img-fluid w-100 figure-img m-0" alt="gallery image {{ $loop->iteration }}">
-                        </a>
-                        <figcaption class="figure-caption" style="font-size: 8px;">
-                            {{ __('Photo by') }} - <a href="https://www.instagram.com/explore/tags/summerstoryrobert/"
-                                          target="_blank"
-                                          class="link-secondary">#summerstoryrobert</a>
-                        </figcaption>
-                    </figure>
-                </div>
-            @endforeach
-        </div>
-    </div>
-</div>
-
-<div class="container"
-     @resize.window.debounce="window.innerWidth >= 992 ? $('#flipbook').turn('display', 'double') : $('#flipbook').turn('display', 'single')"
-     x-data>
-    <div class="row justify-content-center">
-        <div class="col-md-10">
-            <div class="ratio responsive">
-                <div>
-                    <div id="flipbook">
-                        @foreach([
-                                    //'ROB00520.jpg',
-                                    'gallery.jpeg',
-                                    //'ROB00812.jpg',
-                                    'ROB00667.jpg',
-                                    'ROB00398.jpg',
-                                    'ROB00667.jpg',
-                                    'ROB00398.jpg',
-                                    'ROB00667.jpg',
-                                    'ROB00398.jpg',
-                                    'ROB00667.jpg',
-                                    'ROB00398.jpg',
-                                    'ROB00667.jpg',
-                                    'ROB00398.jpg',
-                                    'ROB00667.jpg',
-                                    'ROB00398.jpg',
-                                ] as $gallery)
-                            <div>
-                                <img src="{{ asset('images/gallery/' . $gallery) }}"
-                                     class="img-fluid w-100 user-select-none"
-                                     draggable="false">
-                            </div>
-                        @endforeach
+{{--    <div class="container">--}}
+{{--        <div class="row grid" id="gallery_lightbox">--}}
+{{--            @foreach([--}}
+{{--                'ROB00520.jpg',--}}
+{{--                'gallery.jpeg',--}}
+{{--                'ROB00812.jpg',--}}
+{{--                'ROB00667.jpg',--}}
+{{--                'ROB00398.jpg',--}}
+{{--            ] as $gallery)--}}
+{{--                <div class="col-md-4 grid-item">--}}
+{{--                    <figure class="figure figure-credit">--}}
+{{--                        <a href="{{ asset('images/gallery/' . $gallery) }}"--}}
+{{--                           class="link-secondary text-decoration-none"--}}
+{{--                           data-fancybox="gallery"--}}
+{{--                           data-caption='<span style="font-size: 8px">{{ __('Photo by') }} - <a href="https://www.instagram.com/explore/tags/summerstoryrobert/" target="_blank" class="link-light">#summerstoryrobert</a></span>'>--}}
+{{--                            <img src="{{ asset('images/gallery/' . $gallery) }}"--}}
+{{--                                 class="img-fluid w-100 figure-img m-0" alt="gallery image {{ $loop->iteration }}">--}}
+{{--                        </a>--}}
+{{--                        <figcaption class="figure-caption" style="font-size: 8px;">--}}
+{{--                            {{ __('Photo by') }} - <a href="https://www.instagram.com/explore/tags/summerstoryrobert/"--}}
+{{--                                          target="_blank"--}}
+{{--                                          class="link-secondary">#summerstoryrobert</a>--}}
+{{--                        </figcaption>--}}
+{{--                    </figure>--}}
+{{--                </div>--}}
+{{--            @endforeach--}}
+{{--        </div>--}}
+{{--    </div>--}}
+    <div class="container"
+         @resize.window.debounce="window.innerWidth >= 992 ? $('#flipbook').turn('display', 'double') : $('#flipbook').turn('display', 'single')"
+         x-data>
+        <div class="row justify-content-center">
+            <div class="col-md-10">
+                <div class="ratio responsive">
+                    <div>
+                        <div id="flipbook">
+                            @foreach([
+                                        //'ROB00520.jpg',
+                                        'gallery.jpeg',
+                                        //'ROB00812.jpg',
+                                        'ROB00667.jpg',
+                                        'ROB00398.jpg',
+                                        'ROB00667.jpg',
+                                        'ROB00398.jpg',
+                                        'ROB00667.jpg',
+                                        'ROB00398.jpg',
+                                        'ROB00667.jpg',
+                                        'ROB00398.jpg',
+                                        'ROB00667.jpg',
+                                        'ROB00398.jpg',
+                                        'ROB00667.jpg',
+                                        'ROB00398.jpg',
+                                    ] as $gallery)
+                                <div>
+                                    <img src="{{ asset('images/gallery/' . $gallery) }}"
+                                         class="img-fluid w-100 user-select-none"
+                                         draggable="false">
+                                </div>
+                            @endforeach
+                        </div>
                     </div>
                 </div>
             </div>
