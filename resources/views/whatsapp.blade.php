8c6ea92395c83aabaf8cbf1160b1dd4d710c84cb
Kevin Chandra <kacol.bot@gmail.com>
2022-07-02 23:14:57 +0700

    send whatsapp

---
+++ resources/views/whatsapp.blade.php
@@ -0,0 +1,18 @@
+Dear {{ $guestName }}
+
+You are cordially invited to
+*The Wedding of {{ $groomName }} & {{ $brideName }}*
+
+Together with their families,
+*{{ __('Mr.') }} Tjen Gunawan Chandra & {{ __('Mrs.') }} Susilowati*
+And
+*{{ __('Mr.') }} Ge Cing Kai & {{ __('Mrs.') }} Liauw Hung San*
+
+*Saturday, 24 September 2022*
+*at 18.00*
+*XO Palace*
+Jl. Raya Kupang Indah No.15, Dukuh Kupang, Kec. Dukuhpakis, Kota SBY, Jawa Timur 60225
+
+Please kindly help us prepare everything better by confirming your presence to our wedding event with the following RSVP form:
+{{ $linkToSite }}
+Please complete this RSVP before *{{ $dueDate }}*.
