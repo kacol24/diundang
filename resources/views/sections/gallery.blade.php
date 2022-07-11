de49e4d2233c33e76e3cbbc9c6eff2e36ba735df
Kevin Chandra <kacol.bot@gmail.com>
2022-07-11 22:48:06 +0700

    fix gallery not inside container

--- resources/views/sections/gallery.blade.php
+++ resources/views/sections/gallery.blade.php
@@ -1,21 +1,21 @@
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-5">
-            <a href="#galleryModal" data-bs-toggle="modal">
-                <img src="{{ asset('images/album-cover-front.jpg') }}"
-                     class="img-fluid w-100 user-select-none"
-                     draggable="false">
-            </a>
+<div id="gallery" class="py-3">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-5">
+                <a href="#galleryModal" data-bs-toggle="modal">
+                    <img src="{{ asset('images/album-cover-front.jpg') }}"
+                         class="img-fluid w-100 user-select-none"
+                         draggable="false">
+                </a>
+            </div>
         </div>
     </div>
-</div>
-<style>
-    #gallery .btn-close {
-        opacity: 1;
-        background: transparent url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='%23fff'%3e%3cpath d='M.293.293a1 1 0 011.414 0L8 6.586 14.293.293a1 1 0 111.414 1.414L9.414 8l6.293 6.293a1 1 0 01-1.414 1.414L8 9.414l-6.293 6.293a1 1 0 01-1.414-1.414L6.586 8 .293 1.707a1 1 0 010-1.414z'/%3e%3c/svg%3e") center/1em auto no-repeat !important;
-    }
-</style>
-<div id="gallery" class="py-3">
+    <style>
+        #gallery .btn-close {
+            opacity: 1;
+            background: transparent url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='%23fff'%3e%3cpath d='M.293.293a1 1 0 011.414 0L8 6.586 14.293.293a1 1 0 111.414 1.414L9.414 8l6.293 6.293a1 1 0 01-1.414 1.414L8 9.414l-6.293 6.293a1 1 0 01-1.414-1.414L6.586 8 .293 1.707a1 1 0 010-1.414z'/%3e%3c/svg%3e") center/1em auto no-repeat !important;
+        }
+    </style>
     <div class="modal fade" id="galleryModal" tabindex="-1" aria-labelledby="galleryModalLabel" aria-hidden="true"
          data-bs-backdrop="static">
         <div class="modal-dialog modal-dialog-centered modal-xl">
