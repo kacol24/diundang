612b32b72c22254421d3ec09eb26a32ca0e9e06f
Kevin Chandra <kacol.bot@gmail.com>
2022-07-16 13:44:42 +0700

    favicon and logo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,6 +13,14 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0.27/dist/fancybox.min.css">
     <link rel="stylesheet" href="{{ mix('css/main.css') }}">
 
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#09232f">
+    <meta name="msapplication-TileColor" content="#09232f">
+    <meta name="theme-color" content="#09232f">
+
     <title>The Wedding of Kevin & Fernanda</title>
     <meta name="description"
           content="Dear {{ $guestName }}, you are cordially invited to celebrate the wedding of Kevin and Fernanda on September 24th, 2022.">
