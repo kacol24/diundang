d5d5e37b78372a5e291f0d4bb77fed1e8bc9cc20
Kevin Chandra <kacol.bot@gmail.com>
2022-07-18 15:38:33 +0700

    qr button size

--- resources/views/livewire/floating-qr-button.blade.php
+++ resources/views/livewire/floating-qr-button.blade.php
@@ -1,7 +1,7 @@
 <div>
     @if($isShown)
         <div class="position-fixed" style="bottom: 15px;left: 15px;z-index:1050">
-            <button class="btn d-flex align-items-center rounded-pill shadow-sm" style="background-color: #eee;"
+            <button class="btn d-flex align-items-center rounded-pill shadow-sm" style="background-color: #eee; height: 40px;"
                     data-bs-toggle="modal"
                     data-bs-target="#qrModal">
         <span class="d-flex justify-content-center align-items-center me-2 text-black">
