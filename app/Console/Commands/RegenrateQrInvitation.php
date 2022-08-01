2ee220172d38750bfc1bc65f48b7a381d8751eba
Kevin Chandra <kacol.bot@gmail.com>
2022-08-01 17:40:36 +0700

    added command to generate qr invitation

---
+++ app/Console/Commands/RegenrateQrInvitation.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\GenerateQrCode;
+use App\Jobs\GenerateQrInvitation;
+use App\Models\Invitation;
+use Illuminate\Console\Command;
+
+class RegenrateQrInvitation extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'generate:regenerate-qr-invitations';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Re-generates QR Invitation';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $invitations = Invitation::all();
+
+        foreach ($invitations as $invitation) {
+            $guestCode = $invitation->guest_code;
+            $this->info('Processing guest: '. $guestCode);
+
+            $invitationPath = storage_path("app/public/{$guestCode}.jpg");
+            $this->info('Generating for guest: '.$guestCode);
+            GenerateQrInvitation::dispatchSync($invitation);
+        }
+    }
+}
