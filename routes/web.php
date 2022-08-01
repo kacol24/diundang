799c0ca2743e82c3c43705764595f58a24990b91
Kevin Chandra <kacol.bot@gmail.com>
2022-08-01 17:46:32 +0700

    download invitation route

--- routes/web.php
+++ routes/web.php
@@ -37,61 +37,19 @@ Route::get('/', function () {
 })->name('home');
 
 Route::get('/download', function () {
-    $guestName = '12345678901234567890123456789012345678901234'; // 44 max
-    $guestName = 'Soo Jong Kiau, Ko Nanang, Ko Ati, Ko Aliong, Ko Afo';
-    $guestCode = '616467';
-    $fontSize = 45 - (round(strlen($guestName) / 10) * 5);
-    //dd($fontSize);
-
-    $intervention = new ImageManager;
-    $template = $intervention->make(public_path('images/template-2.jpg'));
-    $template->insert(storage_path('app/public/qr/616467.png'), 'top-left', 350, 525);
-    $template->text($guestName, 600, 1100, function ($font) use ($guestName, $fontSize) {
-        $font->file(public_path('fonts/MADETOMMY-Bold.ttf'));
-        $font->size($fontSize);
-        $font->align('center');
-        $font->valign('middle');
-    });
-    $template->text($guestCode, 600, 1140, function ($font) {
-        $font->file(public_path('fonts/MADETOMMY.ttf'));
-        $font->size(20);
-        $font->align('center');
-        $font->valign('middle');
-    });
-    $template->text('Table: Arendelle', 600, 1180, function ($font) {
-        $font->file(public_path('fonts/MADETOMMY.ttf'));
-        $font->size(20);
-        $font->align('center');
-        $font->valign('middle');
-    });
-    $template->save(storage_path('app/public/616467.jpg'));
-    dd($template);
-
     $guestCode = request('guest');
-    abort_unless($guestCode, 404, 'Guest code missing.');
+    $invitation = Invitation::firstWhere('guest_code', $guestCode);
 
-    $invitation = Invitation::where('guest_code', $guestCode)->firstOrFail();
-
-    $qrname = storage_path('app/public/qr/'.$guestCode.'.png');
-    $filename = storage_path('app/public/['.$guestCode.'] Invitation, The Wedding of Kevin & Fernanda, 09-10-2022.pdf');
-
-    if (! file_exists($filename)) {
-        QrCode::size(500)
-              ->format('png')
-              ->generate($invitation->guest_code, $qrname);
-        $qr = $qrname;
-
-        $pdf = App::make('dompdf.wrapper');
-        $pdf->loadView('qr', compact('invitation', 'qr'));
-        $pdf->save($filename);
-    }
-
-    return response()->download($filename);
+    return response()
+        ->download(
+            $invitation->qr_invitation_path,
+            "[$guestCode] Invitation, The Wedding of Kevin & Fernanda, 24-09-2022.jpg"
+        );
 })->name('download');
 
 Route::post('rsvp', [RsvpController::class, 'store'])
      ->name('rsvp.store');
 
-Route::get('login', function (){
+Route::get('login', function () {
     return redirect()->route('filament.auth.login');
 })->name('login');
