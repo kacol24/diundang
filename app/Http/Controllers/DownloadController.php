c5829f9680b8145ffe0f3d2389299bfc2cda098d
Kevin Chandra <kacol.bot@gmail.com>
2022-08-02 23:16:46 +0700

    optimize for production

---
+++ app/Http/Controllers/DownloadController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Invitation;
+use Illuminate\Http\Request;
+
+class DownloadController extends Controller
+{
+    public function __invoke()
+    {
+        $guestCode = request('guest');
+        $invitation = Invitation::firstWhere('guest_code', $guestCode);
+
+        return response()
+            ->download(
+                $invitation->qr_invitation_path,
+                "[$guestCode] Invitation, The Wedding of Kevin & Fernanda, 24-09-2022.jpg"
+            );
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DownloadController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RsvpController;
 use App\Models\Invitation;
 use Illuminate\Support\Facades\Route;
@@ -15,38 +17,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    $guestName = request('for', 'Honored Guest');
+Route::get('/', HomeController::class)
+     ->name('home');
 
-    $guestCode = request('guest');
-    $invitation = Invitation::firstWhere('guest_code', $guestCode);
-
-    if ($invitation) {
-        $guestName = $invitation->name;
-    }
-
-    $data = [
-        'guestName' => $guestName,
-        'invitation' => $invitation,
-    ];
-
-    return view('home', $data);
-})->name('home');
-
-Route::get('/download', function () {
-    $guestCode = request('guest');
-    $invitation = Invitation::firstWhere('guest_code', $guestCode);
-
-    return response()
-        ->download(
-            $invitation->qr_invitation_path,
-            "[$guestCode] Invitation, The Wedding of Kevin & Fernanda, 24-09-2022.jpg"
-        );
-})->name('download');
+Route::get('/download', DownloadController::class)
+     ->name('download');
 
 Route::post('rsvp', [RsvpController::class, 'store'])
      ->name('rsvp.store');
 
-Route::get('login', function () {
-    return redirect()->route('filament.auth.login');
-})->name('login');
+Route::get('login', [HomeController::class, 'login'])
+     ->name('login');
