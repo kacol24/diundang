d2c0f06be446b1341b6325ff300b6ff16f7eff0d
Kevin Chandra <kacol.bot@gmail.com>
2022-08-05 15:38:16 +0700

    show qr invitation if invitation is set

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -134,7 +134,9 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body p-0">
-                <img src="{{ asset("storage/$invitation?->guest_code.jpg") }}" alt="qr code" class="img-fluid w-100">
+                @if($invitation)
+                    <img src="{{ asset("storage/$invitation?->guest_code.jpg") }}" alt="qr code" class="img-fluid w-100">
+                @endif
             </div>
             <div class="modal-footer justify-content-start">
                 <small class="fst-italic">
