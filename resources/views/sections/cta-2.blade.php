e204fae54290f87216cb767cb34ba19d01a1f0e6
Kevin Chandra <kacol.bot@gmail.com>
2022-08-12 22:26:42 +0700

    cta padding

--- resources/views/sections/cta-2.blade.php
+++ resources/views/sections/cta-2.blade.php
@@ -1,8 +1,8 @@
 <div id="cta" class="darkened-overlay"
-     style="background-repeat: no-repeat; background-size: cover; background-position: center bottom; padding-bottom: 25%"
+     style="background-repeat: no-repeat; background-size: cover; background-position: center bottom"
      data-parallax
      data-src="{{ asset('images/JES03644-4by3.jpg') }}">
-    <div class="container text-white py-5 my-5 mt-0 text-center position-relative font-serif" style="z-index: 2">
+    <div class="container text-white py-5 my-5 mt-0 py-0 py-md-5 text-center position-relative font-serif" style="z-index: 2">
         <h3 class="h4 text-uppercase">
             We Can't Wait To See You!
         </h3>
