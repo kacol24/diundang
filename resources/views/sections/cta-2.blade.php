a03f11611d887c1c209da81bad6f55e112c601ec
Kevin Chandra <kacol.bot@gmail.com>
2022-08-12 22:20:39 +0700

    cta image

--- resources/views/sections/cta-2.blade.php
+++ resources/views/sections/cta-2.blade.php
@@ -1,7 +1,7 @@
 <div id="cta" class="darkened-overlay"
      style="background-repeat: no-repeat; background-size: cover; background-position: center bottom; padding-bottom: 15%"
      data-parallax
-     data-src="{{ asset('images/JES03644.jpg') }}">
+     data-src="{{ asset('images/JES03644-4by3.jpg') }}">
     <div class="container text-white py-5 my-5 mt-0 text-center position-relative font-serif" style="z-index: 2">
         <h3 class="h4 text-uppercase">
             We Can't Wait To See You!
