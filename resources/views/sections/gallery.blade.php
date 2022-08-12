318a17fd586fb39fb102db167b61970682725ef0
Kevin Chandra <kacol.bot@gmail.com>
2022-08-12 21:52:41 +0700

    cover ratio

--- resources/views/sections/gallery.blade.php
+++ resources/views/sections/gallery.blade.php
@@ -101,7 +101,7 @@
         }
 
         .ratio.responsive {
-            --bs-aspect-ratio: {{ 3/2.127 * 100 }}%;
+            --bs-aspect-ratio: {{ 3/2.12 * 100 }}%;
         }
 
         @media (min-width: 992px) {
