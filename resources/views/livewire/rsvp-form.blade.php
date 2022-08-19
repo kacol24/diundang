cfc9ab1762616430dc4a414fff6848c4de9e8748
Kevin Chandra <kacol.bot@gmail.com>
2022-08-19 20:31:26 +0700

    move outside floating qr

--- resources/views/livewire/rsvp-form.blade.php
+++ resources/views/livewire/rsvp-form.blade.php
@@ -123,34 +123,4 @@
             </em>
         </small>
     </form>
-    <div class="modal fade" id="qrModal" tabindex="-1" aria-labelledby="qrModalLabel" aria-hidden="true"
-         data-bs-backdrop="static">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="qrModalLabel">Digital Invitation</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal"
-                            aria-label="Close"></button>
-                </div>
-                @if($invitation)
-                    <div class="modal-body p-0">
-                        <img
-                            src="{{ asset("storage/{$invitation['guest_code']}.jpg") }}?v={{ now()->timestamp }}"
-                            alt="qr code"
-                            class="img-fluid w-100">
-                    </div>
-                    <div class="modal-footer justify-content-start">
-                        <small class="fst-italic">
-                            You can <a
-                                href="{{ route('download', ['guest' => $invitation['guest_code']]) }}"
-                                target="_blank"
-                                style="color: var(--color-secondary)">download</a> this digital invitation,
-                            screenshot
-                            this page, or save the QR Code as image.
-                        </small>
-                    </div>
-                @endif
-            </div>
-        </div>
-    </div>
 </div>
