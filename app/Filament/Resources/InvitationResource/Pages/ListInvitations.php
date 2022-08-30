f12992ab00455f600c6fbee0e49fb7a780281194
Kevin Chandra <kacol.bot@gmail.com>
2022-08-30 23:34:29 +0700

    implement fast paginate

--- app/Filament/Resources/InvitationResource/Pages/ListInvitations.php
+++ app/Filament/Resources/InvitationResource/Pages/ListInvitations.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\InvitationResource\Pages;
 use App\Filament\Resources\InvitationResource;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Tables\Filters\Layout;
+use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListInvitations extends ListRecords
 {
@@ -23,5 +25,10 @@ class ListInvitations extends ListRecords
     protected function getTableFiltersFormColumns(): int
     {
         return 6;
+    }
+
+    protected function paginateTableQuery(Builder $query): Paginator
+    {
+        return $query->fastPaginate($this->getTableRecordsPerPage());
     }
 }
